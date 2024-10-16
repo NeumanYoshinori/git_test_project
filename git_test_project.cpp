9038335251c47630470eb61c351c110c71fe3044
K024G <K024G@MSI>
2024-10-16 15:08:53 +0900

    コミットしました

--- git_test_project.cpp
+++ git_test_project.cpp
@@ -12,7 +12,7 @@ int main()
     std::cout << "プレイヤーのほにゃららをアレした\n";
     std::cout << "naniyattendaomae-!\n";
     std::cout << "End Of The World!";
-    std::cout << "naniittenda";
+    std::cout << "Hello heaven!";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
